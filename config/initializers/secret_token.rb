4fee3dceb1b974202cef9b5efc511f4a5e1d3ec2
AhmadKadhim <akadhim13@gmail.com>
2013-07-22 20:49:04 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Twisted::Application.config.secret_key_base = '533aaebd90039458b6f4255349868e75c28fad177a2bdfc42369683495b6baff2a10a0856404c0fd2c60ba8a9c69c2782325687f6558f6ad07295b342be74fbb'
